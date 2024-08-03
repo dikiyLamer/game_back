1c78c15dad1548eccef823eb843074bba23f625d
dikiyLamer124 <gubaev1998ildarick@mail.ru>
2024-08-03 21:28:16 +0300

    Revert "deleted kal1"
    This reverts commit 5a1daaf236a0b4028ae3acef3c27d80315cfc459.

---
+++ docker_postgres_init.sql
@@ -0,0 +1,9 @@
+CREATE USER leonumeno WITH PASSWORD 'leonumeno' CREATEDB;
+CREATE DATABASE leonumeno
+    WITH
+    OWNER = leonumeno
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
