311da1048a80f3042ba5d8a0bea5283e2f64028e
dikiyLamer124 <gubaev1998ildarick@mail.ru>
2024-08-04 13:29:50 +0300

    fixed update and create

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -26,6 +26,21 @@ export const getUser = async (req: Request, res: Response) => {
 
 export const createUser = async (req: Request, res: Response) => {
   try {
+    const user = await usersRepository.findOne({ where: { id: req.body.id } });
+    if (user) {
+      res.send(user);
+    } else {
+      const newUser = await usersRepository.save(req.body as User);
+      res.send(newUser);
+    }
+  } catch (e) {
+    res.writeHead(400, { 'Content-Type': 'text/plain' });
+    res.end(e);
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
     const newUser = await usersRepository.save(req.body as User);
     res.send(newUser);
   } catch (e) {
